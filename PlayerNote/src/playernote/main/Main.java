1168df4c1eedc1906f87526adb9bde48d91719fa
AnshulA011 <anshula011@gmail.com>
2018-07-18 18:01:29 -0400

    Foundation

---
+++ PlayerNote/src/playernote/main/Main.java
@@ -0,0 +1,14 @@
+package playernote.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+
+	public void onEnable() {
+		
+	}
+
+	public void onDisable() {
+		
+	}
+}
