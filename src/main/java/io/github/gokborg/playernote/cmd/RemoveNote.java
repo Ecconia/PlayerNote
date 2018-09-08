1e232eb7f5cc5a4f2eb60ff93cf1d90a64857fb8
Ecconia <github-commit@ecconia.com>
2018-09-08 11:36:09 +0200

    Improving RemoveNote
    - Properly parsing int's.
    - Adding a colored descriptive feedback for every error possible.

--- src/main/java/io/github/gokborg/playernote/cmd/RemoveNote.java
+++ src/main/java/io/github/gokborg/playernote/cmd/RemoveNote.java
@@ -2,12 +2,14 @@ package io.github.gokborg.playernote.cmd;
 
 import java.util.UUID;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
 import io.github.gokborg.playernote.plugin.NoteHandler;
 import io.github.gokborg.playernote.plugin.NotePlugin;
 
 //TODO: Add option and confirm to delete all notes of a player.
+//TODO: Add pagination
 public class RemoveNote extends SubCommand
 {
 	NoteHandler noteHandler;
@@ -22,47 +24,45 @@ public class RemoveNote extends SubCommand
 	{
 		if(args.length == 2)
 		{
+			//TODO: Query all notes instead.
 			UUID uuid = NotePlugin.getPlayerUUID(sender.getServer(), args[0]);
 			if(uuid != null)
 			{
 				if(noteHandler.hasNotes(uuid))
 				{
-					int value = Integer.valueOf(args[1]);
-					if(value >= 1)
+					try
 					{
-						if(noteHandler.getNotes(uuid) != null)
+						int value = Integer.parseInt(args[1]);
+						if(value > noteHandler.getNotes(uuid).size())
 						{
-							if(value > noteHandler.getNotes(uuid).size())
-							{
-								sender.sendMessage("Invalid number!");
-								return;
-							}
-							
-							value -= 1;
-							noteHandler.removeNote(uuid, value);
-							sender.sendMessage("The note has been removed!");
+							sender.sendMessage(ChatColor.RED + "Not that many notes.");
+							return;
 						}
-						else
-						{
-							sender.sendMessage("The player's list is already empty!");
-						}
+						
+						noteHandler.removeNote(uuid, value - 1);
+						sender.sendMessage(ChatColor.GREEN + "The note has been removed.");
 					}
-					else
+					catch(NumberFormatException e)
 					{
-						sender.sendMessage("Not a valid number!");
+						sender.sendMessage(ChatColor.RED + "Not a valid number.");
 						return;
 					}
+				}
+				else
+				{
+					sender.sendMessage(ChatColor.RED + "This player has no notes stored.");
+					return;
 				}
 			}
 			else
 			{
-				sender.sendMessage("Invalid player!");
+				sender.sendMessage(ChatColor.RED + "Player never visited the server.");
 				return;
 			}
 		}
 		else
 		{
-			//TODO: Usage
+			sender.sendMessage(ChatColor.RED + "Usage: /note read <playername> <note number>");
 			return;
 		}
 	}
